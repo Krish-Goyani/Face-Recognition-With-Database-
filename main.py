ee3b347067cac1b32af1a17ea8f892f4876fe8c6
Krish-Goyani <goyani@duck.com>
2024-08-16 17:52:54 +0530

    all functionality added

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from utils import util
 import tkinter as tk
 import cv2
 from PIL import Image, ImageTk
+import face_recognition
+import os
+import subprocess
+import datetime
+
 class App():
     def __init__(self):
 
         self.main_window = tk.Tk()
+        self.main_window.title("Smart Attendance System - Computer Vision")
         self.main_window.geometry("1200x520+350+100")
 
         self.login_button_main_window = util.get_button(self.main_window, "Login", "green", self.login)
@@ -22,10 +28,17 @@ class App():
 
         self.add_webcam(self.webcam_label)
 
+        self.db_dir = "db"
+
+        if not os.path.exists(self.db_dir):
+            os.mkdir(self.db_dir)
+
+        self.log_path = "log.txt"
+
 
     def add_webcam(self, label):
         if "cap" not in self.__dict__:
-            self.cap = cv2.VideoCapture()
+            self.cap = cv2.VideoCapture(0)
 
             self._label = label
             self.process_webcam()
@@ -38,24 +51,92 @@ class App():
         img_ = cv2.cvtColor(self.most_recent_capture_arr, cv2.COLOR_BGR2RGB)
 
         self.most_recent_capture_pil = Image.fromarray(img_)
-        imgtk = ImageTk.PhotoImage(Image=self.most_recent_capture_pil)
+        imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
 
         self._label.imgtk = imgtk
         self._label.configure(image=imgtk)
 
         self._label.after(20, self.process_webcam)
 
-
-
-
-    
-
     def login(self):
-        pass
+        unkown_img_path = "temp.jpg"
+        cv2.imwrite(unkown_img_path, self.most_recent_capture_arr)
+
+        name = str(subprocess.check_output(['face_recognition',self.db_dir,unkown_img_path]))
+        name = name.split(",")[1][:-5]
+        print(name)
+        if name in ['unknown_person', 'no_persons_found']:
+            util.msg_box('Ups...', 'Unknown user. Please register or try again.')
+
+        else:
+            util.msg_box('Welcome back !', f"Welcome, {name}.")
+            with open(self.log_path, 'a') as f:
+                f.write('{},{},in\n'.format(name, datetime.datetime.now()))
+                f.close()
+
+        os.remove(unkown_img_path)
+
     def logout(self):
-        pass
+
+        unkown_img_path = "temp.jpg"
+        cv2.imwrite(unkown_img_path, self.most_recent_capture_arr)
+
+        name = str(subprocess.check_output(['face_recognition',self.db_dir,unkown_img_path]))
+        name = name.split(",")[1][:-5]
+        
+
+        if name in ['unknown_person', 'no_persons_found']:
+            util.msg_box('Ups...', 'Unknown user. Please register or try again.')
+        else:
+            util.msg_box('Have a nice day!', 'Goodbye, {}.'.format(name))
+            with open(self.log_path, 'a') as f:
+                f.write('{},{},out\n'.format(name, datetime.datetime.now()))
+                f.close()
+
+        os.remove(unkown_img_path)
+
+
     def register_new_user(self):
-        pass
+
+        self.register_new_user_window = tk.Toplevel(self.main_window)
+        self.register_new_user_window.geometry("1200x520+370+120")
+
+        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, "Accept", "green", self.accept_register_new_user)
+        self.accept_button_register_new_user_window.place(x=750, y=300)
+
+        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, "Try Again", "red", self.try_again_register_new_user)
+        self.try_again_button_register_new_user_window.place(x=750, y=400)
+
+        self.capture_label = util.get_img_label(self.register_new_user_window)
+        self.capture_label.place(x=10, y=0, width=700, height=500)
+
+        self.add_img_to_label(self.capture_label)
+
+        self.entry_text_register_new_user = util.get_entry_text(self.register_new_user_window)
+        self.entry_text_register_new_user.place(x=750, y=150)
+
+        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window, 'Please, enter username:')
+        self.text_label_register_new_user.place(x=750, y=70)
+    
+    def add_img_to_label(self, label):
+        imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
+        label.imgtk = imgtk
+        label.configure(image = imgtk)
+
+        self.register_new_user_capture = self.most_recent_capture_arr.copy()
+
+    def accept_register_new_user(self):
+        name = self.entry_text_register_new_user.get(1.0, "end-1c")
+
+        cv2.imwrite(os.path.join(self.db_dir, f"{name}.jpg"), self.register_new_user_capture)
+
+        util.msg_box('Success!', 'User was registered successfully !')
+
+        self.register_new_user_window.destroy()
+
+    def try_again_register_new_user(self):
+        self.register_new_user_window.destroy()
+
 
     def start(self):
         self.main_window.mainloop()
--- utils/util.py
+++ utils/util.py
@@ -1,74 +1,64 @@
-import os
-import pickle
-
 import tkinter as tk
 from tkinter import messagebox
-import face_recognition
-
+from tkinter import font as tkfont
 
 def get_button(window, text, color, command, fg='white'):
-    button = tk.Button(
-                        window,
-                        text=text,
-                        activebackground="black",
-                        activeforeground="white",
-                        fg=fg,
-                        bg=color,
-                        command=command,
-                        height=2,
-                        width=20,
-                        font=('Helvetica bold', 20)
-                    )
+    button = button = tk.Button(
+        window,
+        text=text,
+        activebackground="#333333",
+        activeforeground="#FFFFFF",
+        fg=fg,
+        bg=color,
+        command=command,
+        height=2,
+        width=20,
+        font=('Helvetica', 20, 'bold'),
+        relief=tk.RAISED,
+        borderwidth=3,
+        padx=10,
+        pady=5
+    )
 
     return button
 
 
 def get_img_label(window):
-    label = tk.Label(window)
-    label.grid(row=0, column=0)
+    label = tk.Label(window, bg="#F0F0F0")
+    label.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
     return label
 
 
 def get_text_label(window, text):
-    label = tk.Label(window, text=text)
-    label.config(font=("sans-serif", 21), justify="left")
+    label = tk.Label(window, text=text, bg="#F0F0F0")
+    label.config(font=("Roboto", 21), justify="left", padx=10, pady=5)
     return label
 
 
 def get_entry_text(window):
-    inputtxt = tk.Text(window,
-                       height=2,
-                       width=15, font=("Arial", 32))
+    inputtxt = tk.Text(
+        window,
+        height=2,
+        width=15,
+        font=("Arial", 32),
+        bg="#FFFFFF",
+        fg="#333333",
+        insertbackground="#333333",
+        relief=tk.SUNKEN,
+        borderwidth=2,
+        padx=5,
+        pady=5
+    )
     return inputtxt
 
 
 def msg_box(title, description):
+    root = tk.Tk()
+    root.withdraw()
+    custom_font = tkfont.Font(family="Helvetica", size=12)
+    root.option_add("*Font", custom_font)
+    root.option_add("*Background", "#F0F0F0")
+    root.option_add("*Foreground", "#333333")
     messagebox.showinfo(title, description)
 
 
-def recognize(img, db_path):
-    # it is assumed there will be at most 1 match in the db
-
-    embeddings_unknown = face_recognition.face_encodings(img)
-    if len(embeddings_unknown) == 0:
-        return 'no_persons_found'
-    else:
-        embeddings_unknown = embeddings_unknown[0]
-
-    db_dir = sorted(os.listdir(db_path))
-
-    match = False
-    j = 0
-    while not match and j < len(db_dir):
-        path_ = os.path.join(db_path, db_dir[j])
-
-        file = open(path_, 'rb')
-        embeddings = pickle.load(file)
-
-        match = face_recognition.compare_faces([embeddings], embeddings_unknown)[0]
-        j += 1
-
-    if match:
-        return db_dir[j - 1][:-7]
-    else:
-        return 'unknown_person'
